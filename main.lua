4b44733490a54a9d5f892483f2795549e89e770a
wukelang <lukey1321@gmail.com>
2025-10-27 21:22:34 +1300

    Key maps + update loop

--- main.lua
+++ main.lua
@@ -5,45 +5,63 @@ function love.load()
 end
 
 -- vertices = {100, 100, 100, 200, 200, 200, 200, 100}  -- Square
-vertices = {400, 400, 500, 300, 600, 400}  -- Tri
+-- vertices = {400, 400, 500, 300, 600, 400}  -- Tri
 -- vertices = {400, 400, 500, 300, 600, 400, 550, 500, 450, 500}  -- Pentagon
 -- vertices = {100, 100, 100, 200, 200, 100, 200, 200}  -- Tie
 
-selected_color = {1, 1, 1, 1}
-colors = {
-    ["b"] = {0, 0, 1, 1},
-    ["g"] = {0, 1, 0, 1},
-    ["r"] = {1, 0, 0, 1},
-    ["w"] = {1, 1, 1, 1}
+
+current_color = {1, 1, 1, 1}
+debug_color = {1, 1, 1, 1}
+
+local key_map = {
+    w = function()
+        current_color = {1, 1, 1, 1}
+    end,
+    r = function()
+        current_color = {1, 0, 0, 1}
+    end,
+    g = function()
+        current_color = {0, 1, 0, 1}
+    end,
+    b = function()
+        current_color = {0, 0, 1, 1}
+    end,
+    escape = function()
+        love.event.quit()
+    end
 }
+
 local frame = 1
+local time_elapsed = 0
 local input = ""
 local debug = ""
 
 function love.draw()
-    love.graphics.setColor(selected_color)
-    love.graphics.polygon("line", vertices)
+    love.graphics.setColor(current_color)
+    -- love.graphics.polygon("fill", vertices)
+    local square = {(time_elapsed * 10), 100, (time_elapsed * 10), 200, 200, 200, 200, 100}
+    love.graphics.polygon("fill", square)
 
-    
+    love.graphics.setColor(debug_color)
     frame = frame + 1
-    love.graphics.print(frame)
-    love.graphics.print(debug, 0, 100)
-    love.graphics.print(input, 0, 200)
+    local time_display = "time_elapsed: " .. math.floor(time_elapsed)
+
+    love.graphics.print({debug_color, frame})
+    love.graphics.print({debug_color, time_display}, 0, 10)
+    -- love.graphics.print({debug_color, debug}, 0, 10)
+    love.graphics.print({debug_color, input}, 0, 20)
 
 end
 
 
 function love.keypressed(key)
-    input = key
-
-    if colors[key] ~= nil then
-        -- love.graphics.setColor(colors[key])
-        selected_color = colors[key]
-        debug = table.concat(colors[key], ",")
+    if key_map[key] then
+        key_map[key]()
     end
+end
 
 
-    if key == "escape" then
-        love.event.quit()
-    end
+function love.update(dt)
+    time_elapsed = time_elapsed + dt
+    -- print(dt)
 end
