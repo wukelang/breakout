8640fe29a5d5e34e14382ff20ebe9421d448da90
wukelang <lukey1321@gmail.com>
2025-10-28 21:51:46 +1300

    World

--- main.lua
+++ main.lua
@@ -1,67 +1,44 @@
-function love.load()
-    -- image = love.graphics.newImage("cake.jpg")
-    love.graphics.setColor(0,0,0)
-    -- love.graphics.setBackgroundColor(255,255,255)
+local world = love.physics.newWorld(0, 100)
+
+local triangle = {}
+triangle.body = love.physics.newBody(world, 200, 200, 'dynamic')
+triangle.body:setMass(32)
+triangle.shape = love.physics.newPolygonShape(100,100, 200,100, 200,200)
+triangle.fixture = love.physics.newFixture(triangle.body, triangle.shape)
+
+local bar = {}
+bar.body = love.physics.newBody(world, 200, 450, 'static')
+bar.shape = love.physics.newPolygonShape(0,0, 0,20, 400,20, 400,0)
+bar.fixture = love.physics.newFixture(bar.body, bar.shape)
+
+frame = 0
+time = 0
+
+love.draw = function()
+    love.graphics.print("fps: " .. math.floor(frame / time), 0, 0)
+    love.graphics.polygon('line', triangle.body:getWorldPoints(triangle.shape:getPoints()))
+    love.graphics.polygon('line', bar.body:getWorldPoints(bar.shape:getPoints()))
 end
 
--- vertices = {100, 100, 100, 200, 200, 200, 200, 100}  -- Square
--- vertices = {400, 400, 500, 300, 600, 400}  -- Tri
--- vertices = {400, 400, 500, 300, 600, 400, 550, 500, 450, 500}  -- Pentagon
--- vertices = {100, 100, 100, 200, 200, 100, 200, 200}  -- Tie
+love.update = function(dt)
+    frame = frame + 1
+    time = time + dt
 
 
-current_color = {1, 1, 1, 1}
-debug_color = {1, 1, 1, 1}
+    world:update(dt)
 
-local key_map = {
-    w = function()
-        current_color = {1, 1, 1, 1}
-    end,
-    r = function()
-        current_color = {1, 0, 0, 1}
-    end,
-    g = function()
-        current_color = {0, 1, 0, 1}
-    end,
-    b = function()
-        current_color = {0, 0, 1, 1}
-    end,
+    -- print(triangle.body:getWorldPoints(triangle.shape:getPoints()))
+    -- print(triangle.shape:getPoints())
+end
+
+local keymap = {
     escape = function()
         love.event.quit()
     end
 }
 
-local frame = 1
-local time_elapsed = 0
-local input = ""
-local debug = ""
-
-function love.draw()
-    love.graphics.setColor(current_color)
-    -- love.graphics.polygon("fill", vertices)
-    local square = {(time_elapsed * 10), 100, (time_elapsed * 10), 200, 200, 200, 200, 100}
-    love.graphics.polygon("fill", square)
-
-    love.graphics.setColor(debug_color)
-    frame = frame + 1
-    local time_display = "time_elapsed: " .. math.floor(time_elapsed)
-
-    love.graphics.print({debug_color, frame})
-    love.graphics.print({debug_color, time_display}, 0, 10)
-    -- love.graphics.print({debug_color, debug}, 0, 10)
-    love.graphics.print({debug_color, input}, 0, 20)
-
-end
-
-
-function love.keypressed(key)
-    if key_map[key] then
-        key_map[key]()
+love.keypressed = function(key)
+    if keymap[key] then
+        keymap[key]()
     end
-end
-
-
-function love.update(dt)
-    time_elapsed = time_elapsed + dt
-    -- print(dt)
 end
