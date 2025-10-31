757ca95e9724d81e07678f710cada79342f0082b
wukelang <lukey1321@gmail.com>
2025-10-31 15:30:47 +1300

    Entity user data

--- entities/triangle.lua
+++ entities/triangle.lua
@@ -6,5 +6,6 @@ triangle.body:setMass(32)
 triangle.shape = love.physics.newPolygonShape(100,100, 200,100, 200,200)
 triangle.fixture = love.physics.newFixture(triangle.body, triangle.shape)
 triangle.fixture:setRestitution(0.2)
+triangle.fixture:setUserData("triangle")
 
 return triangle
