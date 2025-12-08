e54e973b6469869e63ea2fb24786ba6aa06e4a53
wukelang <lukey1321@gmail.com>
2025-12-08 20:48:27 +1300

    Launch angles

--- entities/ball.lua
+++ entities/ball.lua
@@ -1,6 +1,8 @@
 local world = require('world')
 local state = require('state')
 
+
+
 return function(x_pos, y_pos)
     local entity_min_speed = 500
     local entity_max_speed = 500
@@ -19,25 +21,43 @@ return function(x_pos, y_pos)
     entity.fixture:setUserData(entity)
 
     entity.type = "ball"
+    entity.last_state = nil
 
     entity.draw = function(self)
         local self_x, self_y = self.body:getWorldCenter()
         love.graphics.circle('fill', self_x, self_y, self.shape:getRadius())
     end
 
+    entity.launch = function(self)
+        if state.button_left and state.button_right then
+            entity.body:setLinearVelocity(0, -300)
+        elseif state.button_left then
+            entity.body:setLinearVelocity(-300, -300)
+        elseif state.button_right then
+            entity.body:setLinearVelocity(300, -300)
+        else
+            entity.body:setLinearVelocity(0, -300)
+        end
+
+        entity.last_state = "normal"
+    end
+
     entity.update = function(self, dt)
         if state.ball_standby == true then
-            self.body:setLinearVelocity(0, 0) 
+            self.body:setLinearVelocity(0, 0)
             local posx, posy = self.body:getPosition()
-            self.body:setPosition(state.paddle_center_x, math.floor(state.paddle_center_y - 10))
+            self.body:setPosition(state.paddle_center_x, math.floor(state.paddle_center_y - 20))
+            entity.last_state = "standby"
+
         else
-            if self.body:getLinearVelocity() == 0 then
-                self.body:setLinearVelocity(300, -300)
+            local vel_x, vel_y = self.body:getLinearVelocity()
+            local speed = math.abs(vel_x) + math.abs(vel_y)
+            if entity.last_state == "standby" and state.ball_standby == false then
+                self.launch()
             end
 
             local vel_x, vel_y = self.body:getLinearVelocity()
             local speed = math.abs(vel_x) + math.abs(vel_y)
-
             local vel_x_is_critical = math.abs(vel_x) > entity_max_speed
             local vel_y_is_critical = math.abs(vel_y) > entity_max_speed
             local vel_x_too_slow = math.abs(vel_x) < entity_min_speed
@@ -53,7 +73,6 @@ return function(x_pos, y_pos)
                 self.body:setLinearVelocity(vel_x, vel_y * 1.01)
             end
 
-            -- print(speed)
             if speed > entity_max_speed then
                 self.body:setLinearDamping(0.4)
             else
@@ -63,6 +82,7 @@ return function(x_pos, y_pos)
         end
 
     end
+
 
 
     return entity
