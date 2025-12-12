f92c1f344ff96cbdbbd8be12f29371a29d5856d2
wukelang <lukey1321@gmail.com>
2025-12-12 22:40:18 +1300

    Gap between bricks, row colors

--- entities.lua
+++ entities.lua
@@ -30,15 +30,22 @@ local entities = {
 }
 
 -- Refactor to use width/height from brick entity
-local row_width = love.window.getMode() - 200
-local row_height = 20
-local brick_width = 60
+local window_width, window_height = love.window.getMode()
+local row_brick_number = 8
+local brick_width = 63
+local brick_height = 23
+local brick_row_width = row_brick_number * brick_width
+local row_padding = window_width - brick_row_width
 
-for i = 0, 39 do 
-    local brick_x = ((brick_width * i) % row_width) + 130
-    local brick_y = (math.floor((brick_width * i) / row_width) * row_height) + 120
+local brick_row_colors = { "red", "orange", "yellow", "green", "cyan" }
+
+for i = 0, 39 do
+    local row_num = math.floor((brick_width * i) / brick_row_width)
+    local brick_x = ((brick_width * i) % brick_row_width) + 180
+    local brick_y = (row_num * brick_height) + 120
+    -- print(row_num)
     -- print(brick_x, brick_y)
-    table.insert(entities, 1, brick(brick_x, brick_y))
+    table.insert(entities, 1, brick(brick_x, brick_y, brick_row_colors[row_num + 1]))
 end
 
 return entities
--- entities/brick.lua
+++ entities/brick.lua
@@ -3,12 +3,14 @@ local state = require('state')
 local textures = require('textures')
 
 local health_color_map = {
-    { outline = {0, 1, 0, 1}, fill = {0, 1, 0, 0.3} },
-    { outline = {1, 1, 0, 1}, fill = {1, 1, 0, 0.3} },
-    { outline = {1, 0, 0, 1}, fill = {1, 0, 0, 0.3} }
+    cyan = { outline = {0, 1, 1, 1}, fill = {0, 1, 1, 0.5} },
+    green = { outline = {0, 1, 0, 1}, fill = {0, 1, 0, 0.5} },
+    yellow = { outline = {1, 1, 0, 1}, fill = {1, 1, 0, 0.5} },
+    orange = { outline = {1, 0.5, 0, 1}, fill = {1, 0.5, 0, 0.5} },
+    red = { outline = {1, 0, 0, 1}, fill = {1, 0, 0, 0.5} }
 }
 
-return function(x_pos, y_pos, health)
+return function(x_pos, y_pos, color, health, variant)
     local entity = {}
     entity.body = love.physics.newBody(world, x_pos, y_pos, 'static')
     entity.shape = love.physics.newRectangleShape(60, 20)
@@ -18,19 +20,17 @@ return function(x_pos, y_pos, health)
 
     entity.type = 'brick'
     entity.health = health or 1
-
+    entity.color = health_color_map[color] or health_color_map.green
 
     entity.draw = function(self)
         local angle = self.body:getAngle()
 
         if self.health > 0 then
-            love.graphics.setColor(health_color_map[self.health].outline or state.default_color)
+            love.graphics.setColor(self.color.fill or state.default_color)
+            love.graphics.polygon('fill', self.body:getWorldPoints(self.shape:getPoints()))
+            love.graphics.setColor(self.color.outline or state.default_color)
             love.graphics.polygon('line', self.body:getWorldPoints(self.shape:getPoints()))
             love.graphics.draw(self.image, x_pos, y_pos, angle, 1, 1, self.image:getWidth()/2, self.image:getHeight()/2)
-
-            love.graphics.setColor(health_color_map[self.health].fill or state.default_color)
-            love.graphics.polygon('fill', self.body:getWorldPoints(self.shape:getPoints()))
-
 
         end
         love.graphics.setColor(state.default_color)
